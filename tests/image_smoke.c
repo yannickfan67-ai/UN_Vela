86717f9b645abd5a41a7f3d7e78e5a68b5da3d11
yannickfan67-ai <yannickfan67@gmail.com>
2026-09-13 06:41:35 -0700

    test: cover minimum signed BMP height

--- tests/image_smoke.c
+++ tests/image_smoke.c
@@ -7,6 +7,16 @@ int main(void){
     if(!vela_image_probe(ppm,sizeof(ppm),&i)||i.width!=2||i.height!=1)return 1;
     if(!vela_image_decode_rgb24(ppm,sizeof(ppm),rgb,sizeof(rgb),&i))return 2;
     if(rgb[0]!=0x11||rgb[1]!=0x22||rgb[2]!=0x33||rgb[3]!=0xAA||rgb[4]!=0xBB||rgb[5]!=0xCC)return 3;
+
+    unsigned char bad_bmp[54]={0};
+    bad_bmp[0]='B';bad_bmp[1]='M';
+    bad_bmp[14]=40;
+    bad_bmp[18]=1;
+    bad_bmp[22]=0x00;bad_bmp[23]=0x00;bad_bmp[24]=0x00;bad_bmp[25]=0x80;
+    bad_bmp[26]=1;
+    bad_bmp[28]=24;
+    if(vela_image_probe(bad_bmp,sizeof(bad_bmp),&i))return 4;
+
     puts("UN_Vela image decoder smoke passed");
     return 0;
 }
